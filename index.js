24944759c00c357b57f1f3377cac3dfe6d47e871
Zac <zacharyflynn06@gmail.com>
2021-05-23 17:40:27 -0600

    commit tree f71cd34fe99fb11fd58dcdbd01d164a9cf7c6b48

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+function saturdayFun(variable = "roller-skate") {
+    return `This Saturday, I want to ${variable}!`
+}
+
+function mondayWork(variable = "go to the office") {
+    return `This Monday, I will ${variable}.`
+}
+
+function wrapAdjective(outterVar= "*") {
+    return function(innerVar= "special") {
+        return `You are ${outterVar}${innerVar}${outterVar}!`
+    }
+}
+
+const Calculator = {
+    add: function(x,y) {
+        return x + y
+    },
+    subtract: function(x,y) {
+        return x - y
+    },
+    multiply: function(x,y) {
+        return x * y
+    },
+    divide: function(x,y) {
+        return x / y;
+    }
+}
+
+const actionApplyer = (int, array) => {
+    for (let i = 0; i < array.length; i++) {
+        int = array[i](int)
+    }
+    return int
+
+}
